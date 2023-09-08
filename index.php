098e33098aee17ed1a2fa6531891dd6c7d26f973
Ya'kub Darussalam <yakub.daruss@cyberdude.com>
2023-09-08 21:07:54 +0700

    add starter file

---
+++ index.php
@@ -0,0 +1,463 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="icon" href="/assets/images/avatar.png">
+    <title>Ya'kub Darussalam</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/animated.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+
+  </head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="js-preloader" class="js-preloader">
+    <div class="preloader-inner">
+      <span class="dot"></span>
+      <div class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+  <!-- ***** Preloader End ***** -->
+
+  <!-- ***** Header Area Start ***** -->
+  <header class="header-area header-sticky wow slideInDown" data-wow-duration="0.75s" data-wow-delay="0s">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <nav class="main-nav">
+            <!-- ***** Logo Start ***** -->
+            <a href="index.html" class="logo">
+              <img src="assets/images/yakub.png" alt="Chain App Dev">
+            </a>
+            <!-- ***** Logo End ***** -->
+            <!-- ***** Menu Start ***** -->
+            <ul class="nav">
+              <li class="scroll-to-section"><a href="#top" class="active">Dashboard</a></li>
+              <li class="scroll-to-section"><a href="#service">Service</a></li>
+              <li class="scroll-to-section"><a href="#experience">Skill & Experience</a></li>
+              <li class="scroll-to-section"><a href="#certified">Certified</a></li>
+              <li class="scroll-to-section"><a href="library.php">Library</a></li></ul>        
+            <a class='menu-trigger'>
+                <span>Menu</span>
+            </a>
+            <!-- ***** Menu End ***** -->
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- ***** Header Area End ***** -->
+
+  <div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="row">
+            <div class="col-lg-6 align-self-center">
+              <div class="left-content show-up header-text wow fadeInLeft" data-wow-duration="1s" data-wow-delay="1s">
+                <div class="row">
+                  <div class="col-lg-10">
+                    
+                  <div style="color : rgb(255, 255, 255);">
+                    <h2>Hi<br>I'm <span class="typing" style="color: rgb(0, 217, 255);"></span><br></h2>
+                  </div>
+                  </div>
+                  <div class="col-lg-12">
+                    <div class="white-button first-button scroll-to-section">
+                      <a href="https://www.linkedin.com/in/yakubdaruss/">Linkedin <i class="fab fa-linkedin"></i></a>
+                    </div>
+                    <div class="white-button scroll-to-section">
+                      <a href="#contact">Github <i class="fab fa-github"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
+                <img src="assets/images/slider-dec-crop-ril-cuy.jpg" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="service" class="services section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 offset-lg-2">
+          <div class="section-heading  wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">
+            <h4>What Can <em>I</em> Do</h4>
+            <img src="assets/images/heading-line-dec.png" alt="">
+            <p>Greatest Work Comes From Sincerity </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 ">
+          <div class="service-item first-service">
+            <div class="icon"></div>
+            <h4>Network Administration</h4>
+            <p>Provide Configuration of Network Devices by Implementing Features and Functions of Routing Switching and QOS.</p>
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="service-item second-service">
+            <div class="icon"></div>
+            <h4>Network Monitoring</h4>
+            <p class="pb-5">Network Monitoring with Tools Such as The Dude, Cacti, Zabbix, Prtg etc.</p>
+            
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="service-item third-service">
+            <div class="icon"></div>
+            <h4>IT Support</h4>
+            <p>Support Maintenance, Troubleshoot & Installation Onsite or By Remote.</p>
+            
+          </div>
+        </div>
+        <div class="col-lg-3">
+          <div class="service-item fourth-service">
+            <div class="icon"></div>
+            <h4>Server Administration</h4>
+            <p>Manage & Develop Server with Linux & Windows Server.</p>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="experience" class="about-us section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 align-self-center">
+          <div class="section-heading offset-lg-3">
+            <h4>Work Experience</h4>
+            <img src="assets/images/heading-line-dec.png" alt="">
+          </div>
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="experience1-item" style="padding-top: 30px;">
+                <img src="assets/images/netdata.png" alt="" style="width:140px;height:50px;">
+                <h4>PT. Network Data Sistem</h4>
+                <p>Network Engineer</p>
+                <p>Sep 2022 - Now</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="experience2-item" style="padding-top: 20px;"">
+                <img src="assets/images/internetwork.png" alt=""  style="width:120px;height:60px;">
+                <h4>PT. Internetwork Komunikasi Indonesia</h4>
+                <p>Network Operation Center</p>
+                <p>Jan 2021 - Jun 2022</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="experience3-item" style="padding-top: 20px; padding-bottom:50px">
+                <img src="assets/images/cni.png" alt=""  style="width:90px;height:50px;">
+                <h4>PT. Cyber Network Indonesia</h4>
+                <p>Engineer on Site</p>
+                <p>Jun 2020 - Jan 2021</p>
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="experience4-item" style="padding-top: 0px; padding-bottom:30px">
+                <img src="assets/images/mtm.png" alt=""  style="width:90px;height:90px;">
+                <h4>PT. Mitra Telemedia Manunggal</h4>
+                <p>Network Operation Center</p>
+                <p>Oct 2018 - Mar 2020</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="right-image">
+            <img src="assets/images/about-right-dec.png" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div id="project" class="the-clients">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 offset-lg-2">
+          <div class="section-heading">
+            <h4>Project</h4>
+            <img src="assets/images/heading-line-dec.png" alt="">
+          </div>
+        </div>
+        <div class="col-lg-12">
+          <div class="naccs">
+            <div class="grid">
+              <div class="row">
+                <div class="col-lg-7 align-self-center">
+                  <div class="menu">
+                    <div class="box-item active">
+                      <div class="thumb">
+                        <div class="row">
+                          <div class="col-lg-4 col-sm-4 col-12">
+                            <h4>PT. Indonesia Morowali Industrial Park</h4>                          </div>
+                          <div class="jenis col-lg-4 col-sm-4 d-none d-sm-block">
+                            <p>Network & Firewall</p>
+                          </div>
+                          
+                        </div>
+                      </div>
+                    </div>
+                    <div class="box-item">
+                      <div class="thumb">
+                        <div class="row">
+                          <div class="col-lg-4 col-sm-4 col-12">
+                            <h4>PT. Tri Adi Bersama (Anteraja)</h4>
+                          </div>
+                          <div class="col-lg-4 col-sm-4 d-none d-sm-block">
+                            <p class="category">Network</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    
+                    <div class="box-item">
+                      <div class="thumb">
+                        <div class="row">
+                          <div class="col-lg-4 col-sm-4 col-12">
+                            <h4>PT. Gramedia Asri Media</h4>
+                          </div>
+                          <div class="col-lg-4 col-sm-4 d-none d-sm-block">
+                            <span class="category">Network</span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="box-item">
+                      <div class="thumb">
+                        <div class="row">
+                          <div class="col-lg-4 col-sm-4 col-12">
+                            <h4>Badan Meteorologi Klimatologi dan Geofisika</h4>
+                          </div>
+                          <div class="col-lg-4 col-sm-4 d-none d-sm-block">
+                            <span class="category">Network</span>
+                          </div>
+                        
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div> 
+                <div class="col-lg-5">
+                  <ul class="nacc">
+                    <li class="active">
+                      <div>
+                        <div class="thumb">
+                          <div class="row">
+                            <div class="col-lg-12">
+                              <div class="client-content">
+                                <img src="assets/images/imip.png" alt="" style="width:150px;height:100px;">
+                                <p>“Implement Fortigate for Main Gateway & Firewall”</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <div class="thumb">
+                          <div class="row">
+                            <div class="col-lg-12">
+                              <div class="client-content">
+                                <img src="assets/images/anteraja.png" alt="" style="width:120px;height:80px;">
+                                <p>“Provide MikroTik Routers on Branch for Manage Bandwith, Filter Rules Access IP, Web & Application.”</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <div class="thumb">
+                          <div class="row">
+                            <div class="col-lg-12">
+                              <div class="client-content">
+                                <img src="assets/images/gramedia.png" alt="" style="width:150px;height:100px;">
+                                <p>“Implement SDWAN Peplink for Manage Connection Branch to HQ”</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                    <li>
+                      <div>
+                        <div class="thumb">
+                          <div class="row">
+                            <div class="col-lg-12">
+                              <div class="client-content">
+                                <img src="assets/images/bmkg.png" alt="" style="width:90px;height:100px;">
+                                <p>“Support Prefentive Maintenance, Rejuvenation of End-of-Life Distribution, Access Switches, & Troubleshoot End User Problem.”</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                </div>          
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="certified" class="container"><br><br><br><br></div>
+
+  <div id="certified-c" class="container" >
+    <div class="row">
+      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <div class="col-lg-8 offset-lg-2">
+          <div class="section-heading">
+            <h4>Certified</h4>
+            <img src="assets/images/heading-line-dec.png" alt="" style="margin:auto;">
+          </div>
+        </div>
+        <ol class="carousel-indicators">
+          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+          <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+        </ol>
+    <div class="carousel-inner offset-lg-1">
+      
+      <div class="carousel-item active col-lg-10">
+        <div class="pricing-item-regular">
+        <img class="d-block w-75" src="assets/images/jncia.png" alt="First slide" style="margin:auto;">
+      </div>
+      </div>
+      <div class="carousel-item col-lg-10">
+        <div class="pricing-item-regular ">
+        <img class="d-block w-50 h-25" src="assets/images/mtcine.png" alt="Second slide" style="margin:auto;">
+      </div>
+      </div>
+      <div class="carousel-item col-lg-10">
+        <div class="pricing-item-regular ">
+        <img class="d-block w-75" src="assets/images/nse.png" alt="Third slide" style="margin:auto;">
+      </div>
+      </div>
+      <div class="carousel-item col-lg-10">
+        <div class="pricing-item-regular ">
+        <img class="d-block w-75" src="assets/images/cscu.png" alt="Fourth slide" style="margin:auto;">
+      </div>
+      </div>
+      <div class="carousel-item col-lg-10">
+        <div class="pricing-item-regular ">
+        <img class="d-block w-50 h-25" src="assets/images/jwd.jpg" alt="Fifth slide" style="margin:auto;">
+      </div>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+</div>
+<div id="about" class="container"><br><br><br><br></div>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-8 offset-lg-2">
+      <div class="section-heading  wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">
+        <h4>About <em>Me</em></h4>
+        <img src="assets/images/heading-line-dec.png" alt=""><br>
+        <p>Starting a career in the network field since 2018, I have experience in managing home, enterprise & telecommunications scale networks with various brand variants such as Cisco, MikroTik, Juniper, Fortigate, Huawei and others. Currently I work at PT. Network Data System as a Network Engineer.</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+  <footer id="newsletter">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="footer-widget">
+            
+          </div>
+        </div>
+        <div class="col-lg-5">
+          <div class="footer-widget">
+            <br><br><br>
+            <h4>Contact Us</h4>
+            <p>Albert Einstein Street, Beta City, Mars </p>
+            <p><a href="#">skype : yakub.daruss1</a></p>
+            <p><a href="#">info@yakub.my.id</a></p>          </div>
+        </div>
+        <div class="col-lg-12">
+          <div class="copyright-text">
+            <p>Copyright © 2022 Kubs-Tech Inc. All Rights Reserved. 
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+
+  <!-- Scripts -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/animation.js"></script>
+  <script src="assets/js/imagesloaded.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+  <script src="assets/js/typed.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script>
+          var typed = new Typed('.typing',{
+            strings: ["Network Engineer", "Cyber Security Enthusiast"],
+              loop: true,
+              typeSpeed: 65,
+              backSpeed: 65
+            });
+  </script>
+  
+</body>
+</html>
